2a3325729993285882704782c762545806fb0d3c
Kang-Woo Jeong <rkddnnet@naver.com>
2016-04-13 15:34:40 +0900

    ㄹㄹ

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+    var Weight : Double? = 0.0
+    var Height : Double? = 0.0
+    
+    var bmi : Double? {
+        
+        get {
+            
+            if (Weight != nil) && (Height != nil){
+                
+                return Weight! / (Height! * Height!)
+            }
+            else{
+                return nil
+            }
+        }
+    }
+    
+    
     @IBOutlet weak var bmiLabel: UILabel!
     
     @IBOutlet weak var heightTextField: UITextField!
@@ -29,6 +47,39 @@ class ViewController: UIViewController {
     func textFieldShouldReturn(textField: UITextField)->Bool{
         textField.resignFirstResponder()
         return true
+    }
+    
+    //Update UI
+    func updateUI(){
+        if let b = self.bmi {
+            self.bmiLabel.text = String(format: "%4.1f",b)
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        guard let txt: String = textField.text else{
+         return
+        }
+        
+        
+        func conv(numString: String) -> Double? {
+            
+            let result : Double? = NSNumberFormatter().numberFromString(numString)?.doubleValue
+            
+            return result
+        }
+        
+            switch(textField){
+                
+            case heightTextField:
+                self.Height = conv(txt)
+            case weightTextField:
+                self.Weight = conv(txt)
+            default:
+                print("Something Wrong")
+            }
+         updateUI()
     }
 
 }
